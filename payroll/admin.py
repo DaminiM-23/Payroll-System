ce2dfb444f3643e3b7d089a211bba59ac43c9dc1
Damini Mahajan <136096055+DaminiM-23@users.noreply.github.com>
2023-10-24 12:31:27 +0530

    Update admin.py

--- payroll/admin.py
+++ payroll/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import (User_login,Employee_details,Salary)
 
 
-# Register your models here.
+
 admin.site.register(User_login)
 admin.site.register(Employee_details)
 admin.site.register(Salary)
-#admin.site.register(Payslip)
+
 
