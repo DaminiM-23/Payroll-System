3f9779e37fa89d427b9e79723e249aad05cae164
Damini Mahajan <136096055+DaminiM-23@users.noreply.github.com>
2023-11-16 10:57:35 +0530

    nothing much

--- payroll/forms.py
+++ payroll/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from .models import Employee_details ,Salary
 from django.contrib.auth.models import User
 
-#class LoginForm(forms.Form):
-#    uname=forms.CharField(max_length=20)
-#    password= forms.CharField(widget=forms.PasswordInput)
-
 class Employee_details_Form(forms.ModelForm):
     class Meta:
         model = Employee_details
