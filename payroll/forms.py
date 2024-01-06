cb3207003df9acf46d6030369747b84888ca42f5
Damini Mahajan <136096055+DaminiM-23@users.noreply.github.com>
2024-01-06 23:33:13 +0530

    Update forms.py

--- payroll/forms.py
+++ payroll/forms.py
@@ -9,37 +9,75 @@ class Employee_details_Form(forms.ModelForm):
         model = Employee_details
         fields = '__all__'
         widgets={
-             'date_of_birth':forms.DateInput(attrs={'type': 'date'}),
-             'date_of_joining':forms.DateInput(attrs={'type': 'date'}),
-             'date_of_relieving':forms.DateInput(attrs={'type': 'date'}),
-             'date_of_increment':forms.DateInput(attrs={'type': 'date'}),
-             'pay_revised_date':forms.DateInput(attrs={'type': 'date'}),
-             'date_of_retirement':forms.DateInput(attrs={'type': 'date'}),
-             'from_appointment_date':forms.DateInput(attrs={'type': 'date'}),
-             'to_appointment_date':forms.DateInput(attrs={'type': 'date'}),
-             'TA': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+
+             'date_of_birth':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'date_of_joining':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'date_of_relieving':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'date_of_increment':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'date_of_retirement':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'from_appointment_date':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'to_appointment_date':forms.DateInput(attrs={'type': 'date','class': 'form-control'}),
+             'TA': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')],),
              'handicap': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
              'quarter': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
              'senior_citizen': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-             'quarter_rent': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-             'pay_status': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),   
-         
+             'pay_status': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+             'employee_type':forms.Select(attrs={'class': 'form-control',}),
+             'id_no':forms.TextInput(attrs={'class': 'form-control'}),
+             #For the image upload
+             'photo':forms.FileInput(attrs={'class': 'form-control-file',}),
+             #Sign ka new variable
+             'sign':forms.FileInput(attrs={'class': 'form-control-file',}),
+             'title':forms.Select(attrs={'class': 'form-control'}),
+             'middle_name':forms.TextInput(attrs={'class': 'form-control'}),
+             'last_name':forms.TextInput(attrs={'class': 'form-control'}),
+             'first_name':forms.TextInput(attrs={'class': 'form-control'}),
+             'gender':forms.Select(attrs={'class': 'form-control'}),
+             'sequence_no':forms.TextInput(attrs={'class': 'form-control'}),
+             'aadhar_id':forms.TextInput(attrs={'class': 'form-control'}),
+             'pan_no':forms.TextInput(attrs={'class': 'form-control'}),
+             'email':forms.TextInput(attrs={'class': 'form-control'}),
+             'mobile_no':forms.TextInput(attrs={'class': 'form-control'}),
+             'address':forms.TextInput(attrs={'class': 'form-control'}),
+             'designation_nature' :forms.Select(attrs={'class': 'form-control'}),
+             'department':forms.Select(attrs={'class': 'form-control'}),
+             'designation':forms.Select(attrs={'class': 'form-control'}),
+             'appointment':forms.Select(attrs={'class': 'form-control'}),
+             'staff_type':forms.Select(attrs={'class': 'form-control'}),
+             'quarter_type':forms.Select(attrs={'class': 'form-control'}),
+             'bank_acc_no':forms.TextInput(attrs={'class': 'form-control',}),
+             'ifsc_code':forms.TextInput(attrs={'class': 'form-control',}),
+             'bank_name':forms.Select(attrs={'class': 'form-control',}),
+             'bank_branch':forms.TextInput(attrs={'class': 'form-control',}),
+             'epf':forms.TextInput(attrs={'class': 'form-control',}),
+             'gpf_no':forms.TextInput(attrs={'class': 'form-control',}),
+             'dcps_no':forms.TextInput(attrs={'class': 'form-control',}),
+             'rule':forms.Select(attrs={'class': 'form-control',}),
+             'pay_level':forms.Select(attrs={'class': 'form-control',}),
+             'cell_number':forms.Select(attrs={'class': 'form-control',}),
+             'basic':forms.TextInput(attrs={'class': 'form-control',}),
+             'pay_status':forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')],),
+             'grade_pay':forms.TextInput(attrs={'class': 'form-control',}),
+             'HRA':forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')],)
+              }
 
-        }
-        shift_joining = forms.ChoiceField( choices=Employee_details.SHIFT_CHOICES,widget=forms.RadioSelect,required=True)
-        shift_relieving = forms.ChoiceField( choices=Employee_details.SHIFT_CHOICES,widget=forms.RadioSelect,required=True)
 
 
 class Salary_Form(forms.ModelForm):
     class Meta:
         model = Salary
         fields = '__all__'
+        
 
-
+class Staff_Mngt_Form(forms.ModelForm):
+    class Meta:
+        model = Employee_details
+        fields = ['staff_type']
 
 # class demoform_Form(forms.ModelForm):         
 #     class Meta:
 #         model = demo
 #         fields = '__all__'
+
        
 
