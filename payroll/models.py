5c6cd2f41b96b3225dccb777c506996e783443e0
Damini Mahajan <136096055+DaminiM-23@users.noreply.github.com>
2024-01-10 12:31:07 +0530

    Update models.py

--- payroll/models.py
+++ payroll/models.py
@@ -4,17 +4,17 @@ from django.core.validators import FileExtensionValidator
 
 class Employee_details(models.Model):
     #PERSONAL_DETAILSS
-    id_no=models.PositiveIntegerField(unique =True, default=1000)
-    TITLE_CHOICES = [('1', 'Mr.'),('2', 'Mrs.'), ('3', 'Dr.')] 
+    id_no=models.PositiveIntegerField(unique =True, default=1)
+    TITLE_CHOICES = [('', 'Select'),('1', 'Mr.'),('2', 'Mrs.'), ('3', 'Dr.')] 
     title=models.CharField(max_length=1,choices=TITLE_CHOICES,blank=False)
     first_name=models.CharField(max_length=30, default='')
     middle_name=models.CharField(max_length=30, default='')
     last_name=models.CharField(max_length=30, default='')
     date_of_birth = models.DateField()
-    GENDER_CHOICES = [('M', 'Male'),('F', 'Female'), ('O', 'Other')] 
+    GENDER_CHOICES = [('', 'Select'),('M', 'Male'),('F', 'Female'), ('O', 'Other')] 
     gender=models.CharField(max_length=1,choices=GENDER_CHOICES,blank=False)
-    id_no=models.IntegerField(unique =True, default=1)
     EMPLOYEE_TYPE_CHOICES = [
+        ('', 'Select'),
         ('1', 'Permanent'),
         # ('2', 'Permanent Teaching Non-Sanctioned'),
         # ('3', 'Permanent Non-Teaching Excess'),
@@ -29,7 +29,7 @@ class Employee_details(models.Model):
     pan_no= models.CharField(max_length=20 , unique =True ,blank=False)
     email = models.EmailField(max_length=30,default='')
     mobile_no = models.CharField(max_length=20,default='')
-    address=models.CharField(max_length=100,default='')
+    address=models.CharField(max_length=500,default='')
     # photo=models.FileField( default='')
     # sign=models.FileField(default='')
        
@@ -43,110 +43,7 @@ class Employee_details(models.Model):
     to_appointment_date = models.DateField()
 
     #SERVICE_TYEP_DETAILS
-    DESIGNATION_NATURE_CHOICES = [    
-        ('1', 'Permanent'),
-        ('2', 'Permanent Teaching Non-Sanctioned'),
-        ('3', 'Permanent Non-Teaching Excess'),
-        ('4', 'Contractual Non-Teaching'),
-        ('5', 'Contractual Teaching'),
-            ]
-    designation_nature=models.CharField(max_length=20, choices=DESIGNATION_NATURE_CHOICES,blank=False)
 
-    DEPARTMENT_CHOICES = [  
-        ('1', 'ACCOUNTS'),
-        ('2', 'ADMIN'),
-        ('3', 'CHEMISTRY'),
-        ('4', 'CIVIL'),
-        ('5', 'COMPUTER'),
-        ('6', 'ELECTRONICS COMPUTER'),
-        ('7', 'ELECTRICAL'),
-        ('8', 'ESTABLISHMENT'),
-        ('9', 'EXAM'),
-        ('10', 'HOSTEL'),
-        ('11', 'HUMANITIES'),
-        ('12', 'LIBRARY'),
-        ('13', 'MAINTENANCE'),
-        ('14', 'MATHS'),
-        ('15', 'MCA'),
-        ('16', 'MECHANICAL'),
-        ('17', 'PHYSICS'),
-        ('18', 'PRODUCTION'),
-        ('19', 'STRUCTURAL'),
-        ('20', 'TEXTILE'),
-        ('21', 'GYMKHANA'),
-        ('22', 'STORE'),
-        ('23', 'W/M SECURITY'),
-        ('24', 'TPO'),
-        ]
-    department=models.CharField(max_length=20, choices=DEPARTMENT_CHOICES, blank=False)
-    DESIGNATION_CHOICES = [   
-        ('1', 'Professor'),
-        ('2', 'Training and Placement Officer'),
-        ('3', 'Junior Engineer'),
-        ('4', 'System Manager'),
-        ('5', 'CA'),
-        ('6', 'Instructor'),
-        ('7', 'Junior Clerk'),
-        ('8', 'Junior Lab Assistant'),
-        ('9', 'Lab Assistant'),
-        ('10', 'Lab Attendant'),
-        ('11', 'Lecturer'),
-        ('12', 'Lib Attendant'),
-        ('13', 'Library Clerk'),
-        ('14', 'Mali'),
-        ('15', 'Med Officer'),
-        ('16', 'M Supervisor'),
-        ('17', 'OSD'),
-        ('18', 'Others'),
-        ('19', 'Peon'),
-        ('20', 'Senior Clerk'),
-        ('21', 'Head Clerk'),
-        ('22', 'Foreman'),
-        ('23', 'Carpenter'),
-        ('24', 'Adjunct faculty'),
-        ('25', 'Adhoc faculty'),
-        ('26', 'Tenure faculty'),
-        ('27', 'Driver'),
-        ('28', 'Assistant Professor'),
-        ('29', 'Director'),
-        ('30', 'STENO (M)'),
-        ('31', 'D O'),
-        ('32', 'Superintendent'),
-        ('33', 'Assistant Instructor'),
-        ('34', 'Accounts Officer'),
-        ('35', 'System Engineer'),
-        ('36', 'Office Assistant'),
-        ('37', 'Watch Man'),
-        ('38', 'Watch Man'),
-        ('39', 'Hamal'),
-        ('40', 'Sweeper'),
-        ('41', 'Electrician'),
-        ('42', 'Plumber'),
-        ('43', 'System Analyst'),]
-    designation=models.CharField(max_length=20, choices=DESIGNATION_CHOICES,blank=False)
-    APPOINTMENT_CHOICES = [
-        ('0', 'Please Select'),    
-        ('1', 'Regular'),
-        ('2', 'DCPS'),
-        ('3', 'Contractual NO EPF'),
-        ('4', 'Contractual'),
-        ('5', 'Contractual NT'),
-           ]
-    appointment=models.CharField(max_length=1, choices=APPOINTMENT_CHOICES,blank=False)
-    STAFF_TYPE_CHOICES = [    
-        ('1', 'CLASS 1'),
-        ('2', 'CLASS 2'),
-        ('3', 'CLASS 3'),
-        ('4', 'CLASS 4'),
-        ('5', 'OTHER'),
-        ('6', 'CONTRACTUAL'),
-        ('7', 'CONTRACTUAL NT'),
-        ]
-    staff_type=models.CharField(max_length=1, choices=STAFF_TYPE_CHOICES, blank=False)
-    BANK_NAME_CHOICES = [
-        ('0', 'Please Select'),    
-        ('1', 'State Bank of India'),
-        ('2', 'Bank Of Maharashtra'),  ]
 
     # # #BASIC_DETAILS
     TA=models.BooleanField(blank=False)
@@ -156,6 +53,7 @@ class Employee_details(models.Model):
     senior_citizen=models.BooleanField(blank=False)
     
     QUARTER_TYPE_CHOICES = [  
+        ('', 'Select'),
         ('1', 'CLASS 1'),
         ('2', 'CLASS 2'),
         ('3', 'CLASS 3'),
@@ -163,6 +61,7 @@ class Employee_details(models.Model):
          ]
     quarter_type=models.CharField(max_length=1, choices=QUARTER_TYPE_CHOICES, blank=False)
     DESIGNATION_NATURE_CHOICES = [    
+        ('', 'Select'),
         ('1', 'Permanent'),
         ('2', 'Permanent Teaching Non-Sanctioned'),
         ('3', 'Permanent Non-Teaching Excess'),
@@ -172,6 +71,7 @@ class Employee_details(models.Model):
     designation_nature=models.CharField(max_length=20, choices=DESIGNATION_NATURE_CHOICES,blank=False)
 
     DEPARTMENT_CHOICES = [  
+        ('', 'Select'),
         ('1', 'ACCOUNTS'),
         ('2', 'ADMIN'),
         ('3', 'CHEMISTRY'),
@@ -199,6 +99,7 @@ class Employee_details(models.Model):
         ]
     department=models.CharField(max_length=20, choices=DEPARTMENT_CHOICES, blank=False)
     DESIGNATION_CHOICES = [   
+        ('', 'Select'),
         ('1', 'Professor'),
         ('2', 'Training and Placement Officer'),
         ('3', 'Junior Engineer'),
@@ -244,6 +145,7 @@ class Employee_details(models.Model):
         ('43', 'System Analyst'),]
     designation=models.CharField(max_length=20, choices=DESIGNATION_CHOICES,blank=False)
     APPOINTMENT_CHOICES = [
+        ('', 'Select'),
         ('0', 'Please Select'),    
         ('1', 'Regular'),
         ('2', 'DCPS'),
@@ -253,6 +155,7 @@ class Employee_details(models.Model):
            ]
     appointment=models.CharField(max_length=1, choices=APPOINTMENT_CHOICES,blank=False)
     STAFF_TYPE_CHOICES = [    
+        ('', 'Select'),
         ('1', 'CLASS 1'),
         ('2', 'CLASS 2'),
         ('3', 'CLASS 3'),
@@ -272,6 +175,7 @@ class Employee_details(models.Model):
     senior_citizen=models.BooleanField(blank=False)
     
     QUARTER_TYPE_CHOICES = [  
+        ('', 'Select'),
         ('1', 'CLASS 1'),
         ('2', 'CLASS 2'),
         ('3', 'CLASS 3'),
@@ -285,6 +189,7 @@ class Employee_details(models.Model):
     bank_acc_no= models.CharField(max_length=20,blank=False)
     ifsc_code =models.CharField(max_length=20,blank=False)
     BANK_NAME_CHOICES = [
+        ('', 'Select'),
         ('0', 'Please Select'),    
         ('1', 'State Bank of India'),
         ('2', 'Bank Of Maharashtra'),  ]
@@ -296,12 +201,14 @@ class Employee_details(models.Model):
     
     # # #PAY_SCALE_DETAILS
     RULE_CHOICES = [   
+        ('', 'Select'),
         ('1', 'R7'),
         ('2', 'R6'),
         ('3', 'LUMSUM'),
         ('4', 'Daily Wages ')]
     rule=models.CharField(max_length=1, choices=RULE_CHOICES, blank=False)
     PAY_LEVEL_CHOICES = [
+        ('', 'Select'),
         ('1', 'S-1(15000-47600)'),
         ('2', 'S-2(15300-48700)'),
         ('3', 'S-3(16600-52400)'),
@@ -331,6 +238,7 @@ class Employee_details(models.Model):
         ('27','LUMSUM')]
     pay_level=models.CharField(max_length=30, choices=PAY_LEVEL_CHOICES, blank=False)
     CELL_NUMBER_CHOICES = [    
+        ('', 'Select'),
         ('1', '1'),
         ('2', '2'),
         ('3', '3'),
@@ -351,8 +259,6 @@ class Employee_details(models.Model):
     pay_status=models.BooleanField(blank=False)
     grade_pay=models.CharField(max_length=30,blank=False)
 
-    def __str__(self):
-        return f"{self.employee_type} - {self.id}"
    
 class Salary(models.Model):
     Salary=models.IntegerField()
